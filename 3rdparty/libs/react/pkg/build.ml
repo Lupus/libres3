8db5a7a4360e5cb63a4d64ac7078e1015f04e54b
Török Edwin <edwin@skylable.com>
2014-10-13 23:18:23 +0300

    don't build react-top
    it'd require an additional package (compiler-libs)

--- 3rdparty/libs/react/pkg/build.ml
+++ 3rdparty/libs/react/pkg/build.ml
@@ -6,7 +6,6 @@ let () =
   Pkg.describe "react" ~builder:`OCamlbuild [
     Pkg.lib "pkg/META";
     Pkg.lib ~exts:Exts.module_library "src/react";
-    Pkg.lib ~exts:Exts.library "src/react_top";
     Pkg.doc "README.md";
     Pkg.doc "CHANGES.md";
     Pkg.doc "test/breakout.ml";
